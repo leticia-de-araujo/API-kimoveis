adad7840f47946744867b25d537c82b855622485
leticia-de-araujo <leticiadearaujonunes@gmail.com>
2022-08-28 18:02:40 -0300

    build: initial folders and feat AppError

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
-import "reflect-metadata"
-import "express-async-errors"
-import express from "express"
+import 'reflect-metadata';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
+import { AppError, handleError } from './errors/appError';
 
-const app = express()
-app.use(express.json())
+const app = express();
 
+app.use(express.json());
 
-export default app
+//app.use routes
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    handleError(err, res);
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
-import app from "./app";
-import AppDataSource from "./data-source";
+import app from './app';
+import AppDataSource from './data-source';
 
-(async () => {
+const init = async () => {
+  const PORT = process.env.PORT || 3000;
 
-    await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source initialized');
     })
-    
-    app.listen(3000, () => {
-        console.log("Servidor executando")
-    })    
-})()
+    .catch((err) => {
+      console.error('Error during Data Source initialization', err);
+    });
+
+  app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}!`);
+  });
+};
+
+init();
